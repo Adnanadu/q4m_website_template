be29f3c01725b4f74fefcd7b803cdfda4fa4fabc
AdnanAdu <adnanpe5@gmail.com>
2025-04-26 18:36:40 +0530

    added Login and signup button widget

--- lib/feature/homePage/view/pages/home_page.dart
+++ lib/feature/homePage/view/pages/home_page.dart
@@ -24,10 +24,39 @@ class HomePage extends StatelessWidget {
               //search bar
               SearchBarWidget(searchController: searchController),
               //login button
+              LoginAndSignupButtonWidget(),
+
+              //cafe at home image
               
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class LoginAndSignupButtonWidget extends StatelessWidget {
+  const LoginAndSignupButtonWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          minimumSize: Size(double.infinity, 40),
+    
+          backgroundColor: const Color(0xFF2CC457),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+        child: Text('Login / Signup'),
       ),
     );
   }
